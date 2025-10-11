9b2bbaafa881bf0d73f484d3af7e7228d2aa23ea
User <gaje13710@gmail.com>
2025-10-11 12:27:56 +0530

    Readme updated

--- pipeline-configuration/pipeline-config.groovy
+++ pipeline-configuration/pipeline-config.groovy
@@ -193,10 +193,10 @@ pipeline {
                 withCredentials([sshUserPrivateKey(credentialsId: EC2_CREDENTIALS_ID, keyFileVariable: 'SSH_KEY_FILE')]) {
                     script {
                         // 1. Securely copy the correct deployment script to the remote server
-                        sh "scp -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} ./docker_deploy_app.sh ${EC2_USER}@${EC2_HOST}:${DEPLOY_DIR}/docker_deploy_app.sh"
+                        bat "scp -o StrictHostKeyChecking=no -i %SSH_KEY_FILE% .\\docker_deploy_app.sh ${EC2_USER}@${EC2_HOST}:${DEPLOY_DIR}/docker_deploy_app.sh"
 
                         // 2. SSH in, make the script executable, and then run it with parameters
-                        sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} ${EC2_USER}@${EC2_HOST} 'chmod +x ${DEPLOY_DIR}/docker_deploy_app.sh && ${DEPLOY_DIR}/docker_deploy_app.sh ${S3_BUCKET_NAME} ${S3_DEPLOY_FOLDER} ${DEPLOY_DIR}'"
+                        bat "ssh -o StrictHostKeyChecking=no -i %SSH_KEY_FILE% ${EC2_USER}@${EC2_HOST} \"chmod +x ${DEPLOY_DIR}/docker_deploy_app.sh && ${DEPLOY_DIR}/docker_deploy_app.sh ${S3_BUCKET_NAME} ${S3_DEPLOY_FOLDER} ${DEPLOY_DIR}\""
                     }
                 }
             }
